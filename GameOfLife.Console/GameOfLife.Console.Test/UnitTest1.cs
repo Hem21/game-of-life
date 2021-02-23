9b8360a621f2cc8364013a3adea0b9cb5cc0c471
ukmmcc <m.j.m.95@live.com>
2021-02-23 10:28:24 +0000

    Added console and test project

---
+++ GameOfLife.Console/GameOfLife.Console.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLife.Console.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
