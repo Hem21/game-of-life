0ec586181ae5432d004db5ffce52002a11721358
ukmmcc <m.j.m.95@live.com>
2021-03-03 17:08:19 +0000

    Refactor Cell class to boolean status. Refactor CreateGrid to create boolean array. Refactor set cell to take in a grid, a row value and a column value then change cell at selected values to true. Throw an exception if values given are out of range. Update tests to pass with new code

--- GameOfLife.Console/GameOfLife.Console.Test/GameTests.cs
+++ GameOfLife.Console/GameOfLife.Console.Test/GameTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace GameOfLife.Console.Test
 {
@@ -24,7 +25,7 @@ namespace GameOfLife.Console.Test
             Assert.AreEqual(2, grid.GetLength(0));
             Assert.AreEqual(3, grid.GetLength(1));
         }
-
+        
         [TestMethod]
         public void CanCreateCell()
         {
@@ -32,50 +33,101 @@ namespace GameOfLife.Console.Test
 
             Assert.IsNotNull(cell);
         }
-
+        
         [TestMethod]
         public void CanCreateNewCellWithDeadStatus()
         {
+            var cell = new Cell();
 
-            var status = Status.Dead;
-            var cell = new Cell(status);
+            var status = cell.Status;
 
-            Assert.AreEqual("D", cell.ToString());
+            Assert.IsFalse(status);
         }
 
         [TestMethod]
         public void CanCreateNewCellWithAliveStatus()
         {
+            var cell = new Cell
+            {
+                Status = true
+            };
 
-            var status = Status.Alive;
-            var cell = new Cell(status);
+            var status = cell.Status;
 
-            Assert.AreEqual("A", cell.ToString());
+            Assert.IsTrue(status);
         }
 
         [TestMethod]
-        public void WhenCellCreatedWithInvalidStatusThenExceptionIsThrown()
+        public void CreateGridWithLiveCell()
         {
-            //test if exception is thrown with invalid status?
-        }
 
-        //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-        [TestMethod]
-        public void WhenCellHasTwoAliveNeighboursInItsRowThenStaysAliveAfterOneIteration()
-        {
             var game = new Game();
-            var alive = new Cell(Status.Alive);
-            var dead = new Cell(Status.Dead);
+            var grid = game.CreateGrid(2,3);
+            var updatedGrid = Game.SetCell(grid, 0, 0);
 
-            var grid = new Cell[,] { { alive, alive, alive }, { dead, dead, dead } };
+            var expectedGrid = new bool[2,3] { { true, false, false },{ false, false, false } };
 
-            var stayAlive = game.StaysAlive(grid);
-
-            var expectedValue = new Cell[,] { { dead, alive, dead }, { dead, dead, dead } };
-
-            CollectionAssert.AreEqual(expectedValue, stayAlive);
-
-
+            CollectionAssert.AreEqual(expectedGrid, updatedGrid);
+            Assert.ThrowsException<Exception>(() => Game.SetCell(grid, 1000, 5));
         }
+
+        [TestMethod]
+        public void WhenCellSelectIsOutOfRangeThenReturnGridUnchanged()
+        {
+
+            var game = new Game();
+            var grid = game.CreateGrid(2, 3);
+
+            Assert.ThrowsException<Exception>(() => Game.SetCell(grid, 1000, 5));
+        }
+        /*
+                [TestMethod]
+                public void WhenCellCreatedWithInvalidStatusThenExceptionIsThrown()
+                {
+                    //test if exception is thrown with invalid status?
+                }
+
+                [TestMethod]
+                public void WhenCellIsSelectedThenStatusChangesToAlive()
+                {
+                    var game = new Game();
+                    var grid = game.CreateGrid(2,2);
+
+                    var updatedGrid = game.SetCell(Status.Alive, grid, 0, 1);
+
+                    var alive = new Cell(Status.Alive);
+                    var dead = new Cell(Status.Dead);
+                    var expectedValue = new Cell[,] { { dead, alive }, { dead, dead } };
+
+                    CollectionAssert.AreEqual(expectedValue, updatedGrid);
+                }
+
+                [TestMethod]
+                public void WhenCellSelectIsOutOfRangeThenReturnGridUnchanged()
+                {
+                    //test if exception is thrown with invalid status instead??
+                }
+
+                //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+                [TestMethod]
+                public void WhenCellHasOneAliveNeighbourInItsRowThenDiesAfterOneIteration()
+                {
+
+                    var game = new Game();
+                    var grid = game.CreateGrid(2, 3);
+
+                    game.SetCell(Status.Alive, grid, 0, 1);
+                    var gameGrid = game.SetCell(Status.Alive, grid, 0, 2);
+                    var stayAlive = game.StaysAlive(gameGrid);
+
+                    var alive = new Cell(Status.Alive);
+                    var dead = new Cell(Status.Dead);
+                    var expectedGrid = new Cell[,] { { dead, dead, dead }, { dead, dead, dead } };
+
+                    CollectionAssert.AreEqual(expectedGrid, stayAlive);
+
+
+                }
+                */
     }
 }
--- GameOfLife.Console/GameOfLife.Console/Game.cs
+++ GameOfLife.Console/GameOfLife.Console/Game.cs
@@ -7,109 +7,45 @@ namespace GameOfLife.Console
     public class Game
     {
 
-        public Cell[,] CreateGrid(int xAxis, int yAxis)
+        public bool[,] CreateGrid(int xAxis, int yAxis)
         {
-            return new Cell[xAxis, yAxis];
+            return new bool[xAxis, yAxis];
         }
 
-        public void DisplayGrid(Cell[,] grid)
+        public static bool[,] SetCell(bool[,] grid, int selectRowPosition, int selectColumn)
         {
+            int row = selectRowPosition;
+            int column = selectColumn;
+            if (row < 0 || row > grid.GetLength(0) || column < 0 || column > grid.GetLength(1))
+            {
+                throw new Exception("Cell ...");
+            }
 
+            grid[row, column] = true;
+
+            return grid;
+        }
+/*
+        public Cell[,] StaysAlive(Cell[,] grid)
+        {
             var row = grid.GetLength(0);
             var column = grid.GetLength(1);
-
+            //checking row
             for (int i = 0; i < row; i++)
             {
                 for (int j = 0; j < column; j++)
                 {
-                    System.Console.Write(string.Format("{0} ", grid[i, j]));
-                }
-                System.Console.Write(Environment.NewLine + Environment.NewLine);
-            }
-        }
-
-
-        public bool SetCell(Status setAlive, Cell[,] grid)
-        {
-
-            System.Console.WriteLine("Enter row and column number (e.g 3,4) to select live cell");
-
-            string PlayerInput = System.Console.ReadLine();
-            string[] Parts = PlayerInput.Split(',');
-            if (Parts.Length != 2) { return false; }
-            int.TryParse(Parts[0], out int row);
-            int.TryParse(Parts[1], out int column);
-
-            if (row < 0 || row > grid.GetLength(0) || column < 0 || column > grid.GetLength(1))
-            {
-                return false;
-            }
-
-            grid[row, column] = new Cell(setAlive);
-            return true;
-        }
-
-        public Cell[,] SetGridSize()
-        {
-            System.Console.WriteLine("Enter row length and column number (e.g 3,4) to create grid");
-
-            string PlayerInput = System.Console.ReadLine();
-            string[] Parts = PlayerInput.Split(',');
-            int.TryParse(Parts[0], out int row);
-            int.TryParse(Parts[1], out int column);
-
-            var grid = CreateGrid(row, column);
-
-            return grid;
-        }
-
-        public void SetGrid()
-        {
-
-            var newGrid = SetGridSize();
-            Status setAlive = Status.Alive;
-
-            bool @continue = true;
-            while (@continue)
-            {
-                DisplayGrid(newGrid);
-
-                System.Console.Write("Write 'Start' if you want to start the game or 'Add' if you want to continue adding cells >");
-                string playerInput = System.Console.ReadLine();
-
-                if (playerInput.Equals("Start"))
-                {
-                    return;
-                }
-                else if (playerInput.Equals("Add"))
-                {
-                    @continue = SetCell(setAlive, newGrid);
-                }
-                else
-                {
-                    throw new Exception("This command doesn't exist, try again");
-                }
-            }
-
-        }
-
-        public Cell[,] StaysAlive(Cell[,] grid)
-        {
-
-            //checking row
-            for (int i = 0; i < grid.GetLength(0); i++)
-            {
-                for (int j = 0; j < grid.GetLength(1); j++)
-                {
-                    if (grid[i, j].Equals("A") && (grid[i + 1, j].Equals("A")))
+                    var firstValue = grid[i,j].Equals(Status.Alive);
+                    var secondValue = grid[i, j + 1].Equals(Status.Alive);
+                    if (firstValue && secondValue)
                     {
-                        grid.SetValue("D", i);
+                        grid.SetValue(Status.Dead, i, j);
                     }
                 }
             }
 
             return grid;
         }
-
+*/
     }
 }
