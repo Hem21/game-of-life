4ec3f0e11a129f8ce949607a2b6b8342b17cc849
ukmmcc <m.j.m.95@live.com>
2021-03-05 12:44:05 +0000

    Added columnValue parameter and condition to check if iteration numbers match row and column values in CheckRowNeighbors method. Added CheckColumnNeighbor method with same logic as checking row but for columns. Added tests to check if a cell has a neighbor number of 0 to 9.

--- GameOfLife.Console/GameOfLife.Console.Test/GameTests.cs
+++ GameOfLife.Console/GameOfLife.Console.Test/GameTests.cs
@@ -47,9 +47,10 @@ namespace GameOfLife.Console.Test
         [TestMethod]
         public void CanCreateNewCellWithAliveStatus()
         {
-            var cell = new Cell();
-
-            cell.Status = true;
+            var cell = new Cell
+            {
+                Status = true
+            };
 
             var status = cell.Status;
 
@@ -64,7 +65,7 @@ namespace GameOfLife.Console.Test
             var grid = game.CreateGrid(2, 3);
             var updatedGrid = Game.SetCell(grid, 0, 0);
 
-            var expectedGrid = new bool[2, 3] { { true, false, false }, { false, false, false } };
+            var expectedGrid = new[,] { { true, false, false }, { false, false, false } };
 
             CollectionAssert.AreEqual(expectedGrid, updatedGrid);
         }
@@ -80,7 +81,184 @@ namespace GameOfLife.Console.Test
         }
 
         [TestMethod]
-        public void WhenCellHasNoLiveRowNeighbourThenCellDies()
+        public void WhenCellHasNoRowNeighborsThenZeroReturned()
+        {
+            var game = new Game();
+            var grid = game.CreateGrid(1, 3);
+            var setGrid = Game.SetCell(grid, 0, 2);
+            var actualValue = Game.CheckRowNeighbors(0, 2, setGrid);
+
+            var expectedValue = 0;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void WhenCellHasOneRowNeighborsThenOneReturned()
+        {
+            var game = new Game();
+            var grid = game.CreateGrid(1, 3);
+            Game.SetCell(grid, 0, 0);
+            var setGrid = Game.SetCell(grid, 0, 1);
+            var actualValue = Game.CheckRowNeighbors(0, 1, setGrid);
+
+            var expectedValue = 1;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void WhenCellHasTwoRowNeighborsThenTwoReturned()
+        {
+            var game = new Game();
+            var grid = game.CreateGrid(1, 3);
+            Game.SetCell(grid, 0, 0);
+            Game.SetCell(grid, 0, 1);
+            var setGrid = Game.SetCell(grid, 0, 2);
+            var actualValue = Game.CheckRowNeighbors(0, 1, setGrid);
+
+            var expectedValue = 2;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void WhenCellHasNoColumnNeighborsThenZeroReturned()
+        {
+            var game = new Game();
+            var grid = game.CreateGrid(3, 3);
+            var setGrid = Game.SetCell(grid, 1, 1);
+            var actualValue = Game.CheckColumnNeighbors(1, setGrid);
+
+            var expectedValue = 0;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void WhenCellHasOneColumnNeighborsThenOneReturned()
+        {
+            var game = new Game();
+            var grid = game.CreateGrid(3, 3);
+            Game.SetCell(grid, 2, 2);
+            var setGrid = Game.SetCell(grid, 1, 1);
+            var actualValue = Game.CheckColumnNeighbors(2, setGrid);
+
+            var expectedValue = 1;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void WhenCellHasTwoColumnNeighborsThenTwoReturned()
+        {
+            var game = new Game();
+            var grid = game.CreateGrid(3, 3);
+            Game.SetCell(grid, 0, 0);
+            Game.SetCell(grid, 2, 2);
+            var setGrid = Game.SetCell(grid, 1, 1);
+            var actualValue = Game.CheckColumnNeighbors(1, setGrid);
+
+            var expectedValue = 2;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void WhenCellHasThreeColumnNeighborsThenThreeReturned()
+        {
+            var game = new Game();
+            var grid = game.CreateGrid(3, 3);
+            Game.SetCell(grid, 0, 0);
+            Game.SetCell(grid, 2, 1);
+            Game.SetCell(grid, 2, 2);
+            var setGrid = Game.SetCell(grid, 1, 1);
+            var actualValue = Game.CheckColumnNeighbors(1, setGrid);
+
+            var expectedValue = 3;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void WhenCellHasFourColumnNeighborsThenFourReturned()
+        {
+            var game = new Game();
+            var grid = game.CreateGrid(3, 3);
+            Game.SetCell(grid, 0, 0);
+            Game.SetCell(grid, 0, 1);
+            Game.SetCell(grid, 2, 1);
+            Game.SetCell(grid, 2, 2);
+            var setGrid = Game.SetCell(grid, 1, 1);
+            var actualValue = Game.CheckColumnNeighbors(1, setGrid);
+
+            var expectedValue = 4;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void WhenCellHasFiveColumnNeighborsThenFiveReturned()
+        {
+            var game = new Game();
+            var grid = game.CreateGrid(3, 3);
+            Game.SetCell(grid, 0, 0);
+            Game.SetCell(grid, 0, 1);
+            Game.SetCell(grid, 0, 2);
+            Game.SetCell(grid, 2, 1);
+            Game.SetCell(grid, 2, 2);
+            var setGrid = Game.SetCell(grid, 1, 1);
+            var actualValue = Game.CheckColumnNeighbors(1, setGrid);
+
+            var expectedValue = 5;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void WhenCellHasSixColumnNeighborsThenSixReturned()
+        {
+            var game = new Game();
+            var grid = game.CreateGrid(3, 3);
+            Game.SetCell(grid, 0, 0);
+            Game.SetCell(grid, 0, 1);
+            Game.SetCell(grid, 0, 2);
+            Game.SetCell(grid, 2, 0);
+            Game.SetCell(grid, 2, 1);
+            Game.SetCell(grid, 2, 2);
+            var setGrid = Game.SetCell(grid, 1, 1);
+            var actualValue = Game.CheckColumnNeighbors(1, setGrid);
+
+            var expectedValue = 6;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void WhenCellHasSevenNeighborsThenSevenReturned()
+        {
+            var game = new Game();
+            var grid = game.CreateGrid(3, 3);
+            Game.SetCell(grid, 0, 0);
+            Game.SetCell(grid, 0, 1);
+            Game.SetCell(grid, 0, 2);
+            Game.SetCell(grid, 2, 0);
+            Game.SetCell(grid, 2, 1);
+            Game.SetCell(grid, 2, 2);
+            Game.SetCell(grid, 1, 0);
+            var setGrid = Game.SetCell(grid, 1, 1);
+            var columnNeighbors = Game.CheckColumnNeighbors(1, setGrid);
+            var rowNeighbors = Game.CheckRowNeighbors(1, 1, setGrid);
+            var actualValue = columnNeighbors + rowNeighbors;
+
+            var expectedValue = 7;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        /*
+        [TestMethod]
+        public void WhenCellHasNoLiveRowNeighborThenCellDies()
         {
 
             var game = new Game();
@@ -93,9 +271,9 @@ namespace GameOfLife.Console.Test
 
             CollectionAssert.AreEqual(expectedValue, actualValue);
         }
-
+        /*
         [TestMethod]
-        public void WhenCellHasOneRowNeighbourThenCellDies()
+        public void WhenCellHasOneRowNeighborThenCellDies()
         {
 
             var game = new Game();
@@ -111,7 +289,7 @@ namespace GameOfLife.Console.Test
         }
 
         [TestMethod]
-        public void WhenCellHasTwoRowNeighbourThenCellStaysAlive()
+        public void WhenCellHasTwoRowNeighborThenCellStaysAlive()
         {
 
             var game = new Game();
@@ -126,6 +304,7 @@ namespace GameOfLife.Console.Test
 
             CollectionAssert.AreEqual(expectedValue, actualValue);
         }
+        */
     }
 
         
--- GameOfLife.Console/GameOfLife.Console/Game.cs
+++ GameOfLife.Console/GameOfLife.Console/Game.cs
@@ -12,8 +12,8 @@ namespace GameOfLife.Console
 
         public static bool[,] SetCell(bool[,] grid, int selectRowPosition, int selectColumnPosition)
         {
-            int row = selectRowPosition;
-            int column = selectColumnPosition;
+            var row = selectRowPosition;
+            var column = selectColumnPosition;
             if (row < 0 || row > grid.GetLength(0) || column < 0 || column > grid.GetLength(1))
             {
                 throw new Exception("Cell selected is not in range");
@@ -24,114 +24,93 @@ namespace GameOfLife.Console
             return grid;
         }
 
-        public static int CheckCellRowNeighbours(int rowNumber, bool[,] grid)
+        public static int CheckRowNeighbors(int rowNumber, int columnNumber, bool[,] grid)
         {
-            int SumOfRowNeighbours = 0;
+            var sumOfRowNeighbors = 0;
 
-            var numOfColumns = grid.GetLength(0);
-            var rowLength = grid.GetLength(1);
+            var rows = grid.GetLength(0);
+            var columns = grid.GetLength(1);
 
             //checking row
-            for (int i = 0; i < numOfColumns; i++)
+            for (var i = 0; i < rows; i++)
             {
 
-                for (int j = 0; j < rowLength; j++)
+                for (var j = 0; j < columns; j++)
                 {
-                    if (j == rowNumber)
+                    var isNotRow = i != rowNumber;
+                    var isNotColumn = j != columnNumber;
+                    if (isNotRow || isNotColumn) continue;
+                    if (j == 0)
                     {
-
-                        if (j == 0)
+                        var valueAfter = grid[i, j + 1];
+                        if (valueAfter.Equals(true))
                         {
-                            var valueAfter = grid[i, j + 1];
-                            if (valueAfter.Equals(true))
-                            {
-                                SumOfRowNeighbours += 1;
-                            }
+                            sumOfRowNeighbors += 1;
                         }
-                        else if (j == rowLength - 1)
+                    }
+                    else if (j == rows - 1)
+                    {
+                        var valueBefore = grid[i, j - 1];
+                        if (valueBefore.Equals(true))
                         {
-                            var valueBefore = grid[i, j - 1];
-                            if (valueBefore.Equals(true))
-                            {
-                                SumOfRowNeighbours += 1;
-                            }
+                            sumOfRowNeighbors += 1;
                         }
-                        else
+                    }
+                    else
+                    {
+                        var valueBefore = grid[i, j - 1].Equals(true);
+                        var valueAfter = grid[i, j + 1].Equals(true);
+
+                        if (valueBefore.Equals(true) && valueAfter.Equals(true))
                         {
-                            var valueBefore = grid[i, j - 1].Equals(true);
-                            var valueAfter = grid[i, j + 1].Equals(true);
-
-                            if (valueBefore.Equals(true) && valueAfter.Equals(true))
-                            {
-                                SumOfRowNeighbours += 2;
-                                break;
-                            }
-                            else if (valueBefore.Equals(true) || valueAfter.Equals(true))
-                            {
-                                SumOfRowNeighbours += 1;
-                                break;
-                            }
-                            else if (!valueBefore.Equals(true) && !valueAfter.Equals(true))
-                            {
-                                SumOfRowNeighbours += 0;
-                                break;
-                            }
-
+                            sumOfRowNeighbors += 2;
+                            break;
                         }
+                        else if (valueBefore.Equals(true) || valueAfter.Equals(true))
+                        {
+                            sumOfRowNeighbors += 1;
+                            break;
+                        }
+                        else if (!valueBefore.Equals(true) && !valueAfter.Equals(true))
+                        {
+                            sumOfRowNeighbors += 0;
+                            break;
+                        }
+
                     }
                 }
             }
 
-            return SumOfRowNeighbours;
+            return sumOfRowNeighbors;
         }
 
-        public static bool[,] UpdateGrid(bool[,] grid)
+        public static int CheckColumnNeighbors(int columnNumber, bool[,] grid)
         {
-            int sumOfNeighbours = 0;
-            for (int i = 0; i < grid.GetLength(0); i++) 
-            {
-                for (int j = 0; j < grid.GetLength(1); j++)
-                {
-                    sumOfNeighbours = CheckCellRowNeighbours(j, grid);
-                    if (sumOfNeighbours < 2) 
-                    {
-                        grid.SetValue(false, i, j);
-                    }
-                }
-            }
-
-            return grid;
-        }
-
-        /*
-        public static int CheckColumnNeighbours(bool[,] grid)
-        {
-            var row = grid.GetLength(0);
-            var column = grid.GetLength(1);
-            int SumOfColumnNeighbours = 0;
+            var rows = grid.GetLength(0);
+            var columns = grid.GetLength(1);
+            var sumOfColumnNeighbors = 0;
 
             //checking column
-            for (int i = 0; i < row; i++)
+            for (var i = 0; i < columns; i++)
             {
-                for (int j = 0; j < column; j++)
-                {
 
+                for (var j = 0; j < rows; j++)
+                {
+                    if (i != columnNumber) continue;
                     if (i == 0)
                     {
                         var valueAfter = grid[i + 1, j];
                         if (valueAfter.Equals(true))
                         {
-                            SumOfColumnNeighbours += 1;
-                            break;
+                            sumOfColumnNeighbors += 1;
                         }
                     }
-                    else if (i == row - 1)
+                    else if (i == columns - 1)
                     {
                         var valueBefore = grid[i - 1, j];
                         if (valueBefore.Equals(true))
                         {
-                            SumOfColumnNeighbours += 1;
-                            break;
+                            sumOfColumnNeighbors += 1;
                         }
                     }
                     else
@@ -139,21 +118,51 @@ namespace GameOfLife.Console
                         var valueBefore = grid[i - 1, j].Equals(true);
                         var valueAfter = grid[i + 1, j].Equals(true);
 
-                        if (valueBefore.Equals(true))
+                        if (valueBefore.Equals(true) && valueAfter.Equals(true))
                         {
-                            SumOfColumnNeighbours += 1;
+                            sumOfColumnNeighbors += 2;
                         }
-                        else if (valueAfter.Equals(true))
+                        else if (valueBefore.Equals(true) || valueAfter.Equals(true))
                         {
-                            SumOfColumnNeighbours += 1;
+                            sumOfColumnNeighbors += 1;
+                        }
+                        else if (!valueBefore.Equals(true) && !valueAfter.Equals(true))
+                        {
+                            sumOfColumnNeighbors += 0;
                         }
 
                     }
                 }
             }
 
-            return SumOfColumnNeighbours;
+            return sumOfColumnNeighbors;
         }
+
+        /*
+        public static bool[,] UpdateGrid(bool[,] grid)
+        {
+            var columns = grid.GetLength(0);
+            var rows = grid.GetLength(1);
+            var updatedGrid = new bool[columns,rows];
+
+            for (var i = 0; i < columns; i++) 
+            {
+                for (var j = 0; j < rows; j++)
+                {
+                    var sumOfNeighbors = CheckRowNeighbors(j, grid);
+                    //var sumOfColumnNeighbors = CheckCellColumnNeighbors(i, grid);
+                    //var sumOfNeighbors = sumOfRowNeighbors + sumOfColumnNeighbors;
+                    if (sumOfNeighbors < 2) 
+                    {
+                        //create new grid and add new values to it?
+                        updatedGrid.SetValue(false, i, j);
+                    }
+                }
+            }
+
+            return updatedGrid;
+        }
+        
         */
     }
 }
