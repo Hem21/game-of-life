206a7bfb17786a8322d1ea9e935c55b674759c93
Tom Durrant <tomchdurrant@gmail.com>
2021-03-22 17:36:36 +0000

    Add reference to new project and scaffold services

--- GameOfLife.Console/GameOfLife.Service/Startup.cs
+++ GameOfLife.Console/GameOfLife.Service/Startup.cs
@@ -67,7 +67,7 @@ namespace GameOfLife.Service
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
                 }
             });
         }
