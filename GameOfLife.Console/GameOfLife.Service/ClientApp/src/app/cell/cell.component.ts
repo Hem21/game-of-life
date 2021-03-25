7a9ec735028f4f87c1740932ba93cf6fb28a2cbe
Megan McClellan <Megan_McClellan@waters.com>
2021-03-25 16:15:01 +0000

    Add game over method with alert

--- GameOfLife.Console/GameOfLife.Service/ClientApp/src/app/cell/cell.component.ts
+++ GameOfLife.Console/GameOfLife.Service/ClientApp/src/app/cell/cell.component.ts
@@ -2,22 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { take } from "rxjs/operators";
 import { BackEndService, Grid } from "../services/backend.service";
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: "app-cell",
   templateUrl: "./cell.component.html"
 })
-export class CellComponent implements OnInit{
+export class CellComponent implements OnInit {
   private inputValidators = [Validators.required, Validators.min(1), Validators.max(20)];
   public formGroup: FormGroup = new FormGroup({
     rowAngularID: new FormControl("", this.inputValidators),
     columnAngularID: new FormControl("", this.inputValidators)
- });
+  });
   row: number;
   column: number;
   public grid: Grid;
   rowPosition: number;
   columnPosition: number;
+  previousGrid: Grid;
   public hide: boolean = false;
   public show: boolean = true;
 
@@ -39,7 +42,7 @@ export class CellComponent implements OnInit{
   }
 
   createGridClick(rowValue: number, columnValue: number) {
-    if(this.formGroup.valid){
+    if (this.formGroup.valid) {
       this.row = rowValue;
       this.column = columnValue;
       this.showAndHide();
@@ -52,7 +55,7 @@ export class CellComponent implements OnInit{
       this.grid = result;
     });
   }
- 
+
 
   selectCell(rowIndex: number, columnIndex: number) {
     this.rowPosition = rowIndex;
@@ -68,5 +71,15 @@ export class CellComponent implements OnInit{
     })
   }
 
-
+  playGame() {
+    var currentGrid = this.grid;
+    if (JSON.stringify(currentGrid) == JSON.stringify(this.previousGrid)) {
+      Swal.fire('Game Over');
+    } else {
+      this.backendService.updateGrid(this.grid).subscribe(result => {
+        this.grid = result;
+      })
+    }
+    this.previousGrid = currentGrid;
+}
 }
