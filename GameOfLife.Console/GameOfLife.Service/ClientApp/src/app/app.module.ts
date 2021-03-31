53b35fc46091121de2bfbf8080f81125f564f4d1
ukales <audrey_lescot@waters.com>
2021-03-31 10:39:51 +0100

    added meteo component

--- GameOfLife.Console/GameOfLife.Service/ClientApp/src/app/app.module.ts
+++ GameOfLife.Console/GameOfLife.Service/ClientApp/src/app/app.module.ts
@@ -8,9 +8,12 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CellComponent } from './cell/cell.component';
 import { GridSetUpComponent } from './grid/grid.component';
-import { RulesComponent } from './rules/rules.component'
+import { RulesComponent } from './rules/rules.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MeteoComponent } from './meteo/meteo.component';
+
+
 
 @NgModule({
   declarations: [
@@ -18,7 +21,8 @@ import { MatFormFieldModule, MatInputModule } from '@angular/material';
     HomeComponent,
     CellComponent,
     GridSetUpComponent,
-    RulesComponent
+    RulesComponent,
+    MeteoComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -28,6 +32,7 @@ import { MatFormFieldModule, MatInputModule } from '@angular/material';
       { path: "home", component: HomeComponent },
       { path: "grid", component: GridSetUpComponent },
       { path: "rules", component: RulesComponent },
+      {path: "meteo", component: MeteoComponent },
       { path: " ", redirectTo: "home", pathMatch: "full" },
       { path: "**", redirectTo: "home", pathMatch: "full" }
     ], { useHash: true }),
