2cc2fc8b549a831202286f7a59c6d2407325d91f
ukales <audrey_lescot@waters.com>
2021-05-11 08:32:11 +0100

    Added css for carousel

--- GameOfLife.Console/GameOfLife.Service/ClientApp/src/app/tutorial-box/tutorial-box.component.ts
+++ GameOfLife.Console/GameOfLife.Service/ClientApp/src/app/tutorial-box/tutorial-box.component.ts
@@ -8,12 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TutorialBoxComponent {
   images = [
-    { title: 'hello world', image: '../assets/creategrid.gif', content: 'You can check the rules by clicking on this tab'},
-    { title: 'this is the second slide', image: 'https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGljdHVyZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80'},
-    '/some/image/a.png',
-    '/other/image/b.png',
-    '/third/image/c.png',
-    '/more/images/d.png',
+    { title: 'Selected grid', image: '../assets/selectexistinggrid.gif', content: 'You can play with a grid that already exists, you need to select the grid before playing'},
+    { title: 'Create a grid', image: '../assets/creategrid.gif', content: 'Enter the size of your grid to play' },
   ];
  
 }
